bc0d81d122722bc930fdf3608f08055485571096
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-22 18:37:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,12 @@
 def key_for_min_value(name_hash)
   i = 0
   name_hash.collect do |key, value|
-    if value[i] < value[i + 1]
-      
+    if value[i] < value[i + 1] && value[i] < value[i + 2]
+      key[i]
+    elsif value[i + 1] < value[i] && value[i + 1] < value[i + 2]
+      key[i + 1]
+    else value[i + 2] < value[i] && value[i + 2] < value[i + 1]
+      key[i + 2]
     end
   end 
 end
